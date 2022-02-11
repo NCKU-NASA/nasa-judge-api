35a98d64e7ce11adcb211ed75bb95569e1247622
Chummy <57281249+Jimmy01240397@users.noreply.github.com>
2022-02-11 23:41:12 +0800

    Update server.sh

--- server.sh
+++ server.sh
@@ -4,5 +4,5 @@ workdir="/etc/nasajudgeserver"
 
 . ./venv/bin/activate
 
-gunicorn --bind [::]:80 server:app
+gunicorn --bind [::]:80 server:app --timeout 60
 #gunicorn --certfile=server.crt --keyfile=server.key --bind [::]:443 server:app
