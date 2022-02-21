64048b7823bbec7754c772e230f1a30cfd3ced3c
chummy <jimmy012403976@gmail.com>
2022-02-22 05:36:17 +0800

    fix lab01 bug

--- lab/Lab01/external/0.sh
+++ lab/Lab01/external/0.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#if [ $# -lt 1 ]
-#then
-#    echo "usage: $0 <clt ip>"
-#    exit 0
-#fi
+if [ $# -lt 3 ]
+then
+    echo "usage: $0 <wanip> <studentId> <password>"
+    exit 0
+fi
 
-if [ "$(ping 8.8.8.8 -c 1 -W 1 | grep "bytes from 8.8.8.8: icmp_seq=1")" == "" ]
+if [ "$(sshpass -p $3 ssh $2@$1 ping 8.8.8.8 -c 1 -W 1 | grep "bytes from 8.8.8.8: icmp_seq=1")" == "" ]
 then
     echo false
     exit 0
