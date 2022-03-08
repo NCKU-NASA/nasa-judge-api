4a12a6d8824dad69f834537b9466491fa41c58ee
chummy <jimmy012403976@gmail.com>
2022-03-08 12:01:08 +0800

    fix server async

--- lab/Lab02/external/4.sh
+++ lab/Lab02/external/4.sh
@@ -8,7 +8,7 @@ fi
 context=$(sed "s/<wanip>/$1/g" exweb.html | sha256sum | sed "s/[^0-9a-zA-Z]//g")
 
 # testing service is up with curl
-if [ "$(curl -s -w "%{http_code}" -o /dev/null $1:8080)" != "200" ]
+if [ "$(curl --connect-timeout 1 -s -w "%{http_code}" -o /dev/null $1:8080)" != "200" ]
 then
     echo false
     exit 0
@@ -16,16 +16,16 @@ fi
 
 
 # testing services context with curl
-if [ "$(echo "$(curl -s $1:8080)" | sha256sum | sed "s/[^0-9a-zA-Z]//g")" != "$context" ]
+if [ "$(echo "$(curl --connect-timeout 1 -s $1:8080)" | sha256sum | sed "s/[^0-9a-zA-Z]//g")" != "$context" ]
 then
     echo false
     exit 0
 fi
 
 # testing img url with curl
-for imgurl in $(curl -s $1:8080 | grep -oP "(?<=\<img src=)(.*)(?= width=400px>)")
+for imgurl in $(curl --connect-timeout 1 -s $1:8080 | grep -oP "(?<=\<img src=)(.*)(?= width=400px>)")
 do
-    if [ "$(curl -s -w "%{http_code}" -o /dev/null $imgurl)" != "200" ]
+    if [ "$(curl --connect-timeout 1 -s -w "%{http_code}" -o /dev/null $imgurl)" != "200" ]
     then
         echo false
         exit 0
--- server.sh
+++ server.sh
@@ -5,5 +5,5 @@ workdir="/etc/nasajudgeserver"
 . ./venv/bin/activate
 
 #python server.py
-gunicorn --bind [::]:80 server:app --timeout 300
+gunicorn --bind [::]:80 server:app -k gevent --worker-connections 80 --timeout 300
 #gunicorn --certfile=server.crt --keyfile=server.key --bind [::]:443 server:app
