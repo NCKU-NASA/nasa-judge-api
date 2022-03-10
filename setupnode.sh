3745009f8b1ac034ac60b7af041fc7cfa21f41b8
chummy <jimmy012403976@gmail.com>
2022-03-11 06:02:43 +0800

    update setupnode.sh

--- setupnode.sh
+++ setupnode.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+if [ $# -lt 3 ]
+then
+    echo "usage: $0 <wanip> <min> <max>"
+    exit 0
+fi
+
+
 > /etc/nasajudgeapi/node.conf
 
-for a in $(seq 100 1 200)
+for a in $(seq $2 1 $3)
 do 
-    ping 192.168.123.$a -c 1 -w 1 | grep -P "64 bytes from .*: icmp_seq=1 ttl=64" | sed 's/64 bytes from //g' | sed 's/: icmp_seq=1 ttl=64.*//g' >> /etc/nasajudgeapi/node.conf
+    ping $(echo $1 | sed 's/[0-9]*$//g')$a -c 1 -w 1 | grep -P "64 bytes from .*: icmp_seq=1 ttl=64" | sed 's/64 bytes from //g' | sed 's/: icmp_seq=1 ttl=64.*//g' >> /etc/nasajudgeapi/node.conf
 done
 
 
