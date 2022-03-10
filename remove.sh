d9125cf494c253b7d1769d3fe528bc498709baea
Chummy <57281249+Jimmy01240397@users.noreply.github.com>
2022-03-11 04:20:07 +0800

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo systemctl stop nasajudgeapi.service
+sudo systemctl disable nasajudgeapi.service
+
+sudo rm /etc/systemd/system/nasajudgeapi.service
+
+sudo umount /etc/nasajudgeapi/files
+
+for filename in addvpnuser.sh files lab node.conf __pycache__ requirements.txt server.py server.sh setupnode.sh testcheck.py testlab.json venv
+do
+	sudo rm -r /etc/nasajudgeapi/$filename
+done
+
+sudo mv /etc/nasajudgeapi/server.key .
+sudo mv /etc/nasajudgeapi/server.crt .
+
+if [ "`ls /etc/nasajudgeapi`" = "" ]
+then
+	rm -r /etc/nasajudgeapi
+fi
+
+echo ""
+echo ""
+echo "NASA Judge API Service remove.sh complete."
+
+for filename in server.key server.crt
+do
+	echo "Your ${filename} is at $(pwd)/${filename}."
+done
+
+echo "If you don't need then, please delete then."
