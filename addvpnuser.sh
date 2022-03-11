f205ef72576e7d9d4f806aed7d35e2b1ce550538
chummy <jimmy012403976@gmail.com>
2022-03-11 21:56:17 +0800

    fix addvpnuser.sh mtu

--- addvpnuser.sh
+++ addvpnuser.sh
@@ -13,10 +13,12 @@ then
 fi
 
 bash /etc/wireguard/addwguser.sh -s /etc/wireguard/server.conf -u $1 -f nasajudge.chummydns.com -r 0.0.0.0/0 -ns 10.100.100.254
+sed -i '4 aMTU = 1350' /etc/wireguard/client/$1.conf
 
 userip=$(grep -B 1 -A 3 "# $1" /etc/wireguard/server.conf | grep -oP '(?<=AllowedIPs\s=\s)\d+(\.\d+){3}\/.*' | tail -n 1)
 
 bash /etc/wireguard/addwguser.sh -s /etc/wireguard/testserver.conf -u $1 -f nasajudge.chummydns.com -r $userip,10.100.200.254/32 -d client2 -a 10.100.200.$(echo $userip | sed 's/.*\.//g' | sed 's/\/.*//g') -ns 10.100.200.254
+sed -i '4 aMTU = 1350' /etc/wireguard/client2/$1.conf
 
 mkdir /tmp/nasa
 
