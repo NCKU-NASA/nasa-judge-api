68707fc85e41a346912d747e7891a92f25c08d4c
chummy <jimmy012403976@gmail.com>
2022-04-28 05:20:23 +0800

    fix wireguard mtu

--- addvpnuser.sh
+++ addvpnuser.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 if [ $# -lt 1 ]
@@ -12,13 +13,26 @@ then
     exit 0
 fi
 
-bash /etc/wireguard/addwguser.sh -s /etc/wireguard/server.conf -u $1 -f nasajudge.chummydns.com -r 0.0.0.0/0 -ns 10.100.100.254
-sed -i '4 aMTU = 1350' /etc/wireguard/client/$1.conf
+if [ "$(grep "# $1" /etc/wireguard/server.conf)" != "" ]
+then
+    echo "User exist"
+else
+    bash /etc/wireguard/addwguser.sh -s /etc/wireguard/server.conf -u $1 -f nasajudge.chummydns.com -r 0.0.0.0/0 -ns 10.100.100.254
+    sed -i '4 aPostUp = iptables -t mangle -A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu' /etc/wireguard/client/$1.conf
+    sed -i '5 aPostUp = bash -c "for a in \\$(seq 1 1 \\$(iptables-save | grep \\"\\\\\\"wg-quick(8) rule for %i\\\\\\" -j DROP\\" | sed '"'"'s/-A/-D/g'"'"' | wc -l)); do bash -c \\\"iptables -t raw \\$(iptables-save | grep \\\"\\\\\\"wg-quick(8) rule for %i\\\\\\" -j DROP\\\" | sed '"'"'s/-A/-D/g'"'"' | head -n 1)\\\"; done"' /etc/wireguard/client/$1.conf
+    sed -i '6 aPostDown = bash -c "for a in \\$(seq 1 1 \\$(sudo iptables-save | grep \\"\\\\-A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu\\" | wc -l)); do iptables -t mangle -D FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu; done"' /etc/wireguard/client/$1.conf
+    sed -i '7 aMTU = 1420' /etc/wireguard/client/$1.conf
+fi
 
 userip=$(grep -B 1 -A 3 "# $1" /etc/wireguard/server.conf | grep -oP '(?<=AllowedIPs\s=\s)\d+(\.\d+){3}\/.*' | tail -n 1)
 
-bash /etc/wireguard/addwguser.sh -s /etc/wireguard/testserver.conf -u $1 -f nasajudge.chummydns.com -r $userip,10.100.200.254/32 -d client2 -a 10.100.200.$(echo $userip | sed 's/.*\.//g' | sed 's/\/.*//g') -ns 10.100.200.254
-sed -i '4 aMTU = 1350' /etc/wireguard/client2/$1.conf
+if [ "$(grep "# $1" /etc/wireguard/testserver.conf)" != "" ]
+then
+    echo "User exist"
+else
+    bash /etc/wireguard/addwguser.sh -s /etc/wireguard/testserver.conf -u $1 -f nasajudge.chummydns.com -r $userip,10.100.200.254/32 -d client2 -a 10.100.200.$(echo $userip | sed 's/.*\.//g' | sed 's/\/.*//g') -ns 10.100.200.254
+    sed -i '4 aMTU = 1420' /etc/wireguard/client2/$1.conf
+fi
 
 mkdir /tmp/nasa
 
