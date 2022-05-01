34c2cc824c192a9b6544c33dd732ac4f24e25d8d
axwl03 <arcticwarfare2711@gmail.com>
2022-05-02 00:20:13 +0800

    Adjust judge scripts structure for Lab10

---
+++ lab/Lab10/external/can_get_user.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''
+This script checks whether client can login and get user data
+'''
+import requests, sys
+import utils
+
+if len(sys.argv) != 2:
+    print('Usage: ./script <ip>', file=sys.stderr)
+    sys.exit(1)
+ip = sys.argv[1]
+base_url = f'http://{ip}'
+
+for credential in open('external/credential.lst', 'r'):
+    try:
+        username, password = credential.strip().split(':')
+        sess = requests.Session()
+        credential = {'username': username, 'password': password}
+        utils.register(base_url, credential)
+        if utils.login(sess, base_url, credential) == 200:
+            body = utils.get_user(sess, base_url)
+            if isinstance(body, dict) and body['username'] == username:
+                continue
+    except Exception as e:
+        print(e, file=sys.stderr)
+    print('false')
+    sys.exit(1)
+print('true')
---
+++ lab/Lab10/external/can_register_login.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''
+This script checks whether client can register and login
+'''
+import requests, sys
+import utils
+
+if len(sys.argv) != 2:
+    print('Usage: ./script <ip>', file=sys.stderr)
+    sys.exit(1)
+ip = sys.argv[1]
+base_url = f'http://{ip}'
+
+for credential in open('external/credential.lst', 'r'):
+    try:
+        username, password = credential.strip().split(':')
+        sess = requests.Session()
+        credential = {'username': username, 'password': password}
+        utils.register(base_url, credential)
+        if utils.login(sess, base_url, credential) == 200:
+            continue
+    except Exception as e:
+        print(e, file=sys.stderr)
+    print('false')
+    sys.exit(1)
+print('true')
