8adf569106761845c7719c32a0c336e23436fb15
Chummy <57281249+Jimmy01240397@users.noreply.github.com>
2022-11-01 07:19:24 +0800

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -7,7 +7,7 @@ sudo rm /etc/systemd/system/nasajudgeapi.service
 
 sudo umount /etc/nasajudgeapi/files
 
-for filename in addvpnuser.sh db.conf files lab node.conf finaluserlist.conf __pycache__ requirements.txt server.py server.sh setupnode.sh testcheck.py testlab.json venv
+for filename in addvpnuser.sh db.conf files lab node.conf finaluserlist.conf __pycache__ requirements.txt server.py server.sh setupnode.sh testcheck.py testlab.json venv onclearjudge.sh onstartjudge.sh
 do
 	sudo rm -r /etc/nasajudgeapi/$filename
 done
